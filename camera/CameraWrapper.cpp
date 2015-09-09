e0e478918cb156477ef72fcdd2b4f87920f10631
pexcn <pexcn@qq.com>
2015-09-09 18:46:07 +0800

    camera: clean up
    Change-Id: I42e033983ed6863d7ebd116f64df9cee30ac28a7

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -390,8 +390,6 @@ static int camera_dump(struct camera_device *device, int fd)
     return VENDOR_CALL(device, dump, fd);
 }
 
-extern "C" void heaptracker_free_leaked_memory(void);
-
 static int camera_device_close(hw_device_t *device)
 {
     int ret = 0;
@@ -413,9 +411,7 @@ static int camera_device_close(hw_device_t *device)
         free(wrapper_dev->base.ops);
     free(wrapper_dev);
 done:
-#ifdef HEAPTRACKER
-    heaptracker_free_leaked_memory();
-#endif
+
     return ret;
 }
 

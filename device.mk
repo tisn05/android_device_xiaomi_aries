2a2ca2f082c3a0d267fdcd1ec784fff08ca2c420
pexcn <pexcn@qq.com>
2015-09-17 17:18:03 +0800

    clean up
    Change-Id: I60fb27c7b01103ccc61d4e94dd473fbc8c0b27d4

--- device.mk
+++ device.mk
@@ -23,10 +23,6 @@ LOCAL_PATH := device/xiaomi/aries
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# This device is xhdpi. However the platform doesn't
-# currently contain all of the bitmaps at xhdpi density so
-# we do this little trick to fall back to the hdpi version
-# if the xhdpi doesn't exist.
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 

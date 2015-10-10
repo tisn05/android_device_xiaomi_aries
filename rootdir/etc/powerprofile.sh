bb3106adcf1e8d6bcad2ac5840bd31ce71934aac
pexcn <pexcn@qq.com>
2015-10-10 12:51:50 +0800

    update powerprofile.sh
    Change-Id: I13ae0a707c9cd4f92aee3cfe0da13be965d8f5a0

--- rootdir/etc/powerprofile.sh
+++ rootdir/etc/powerprofile.sh
@@ -39,32 +39,32 @@ case "$powermode" in
 		echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 		echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 		echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-		echo 1566000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-		echo 1566000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-		echo 1566000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-		echo 1566000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+		echo 1350000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+		echo 1350000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+		echo 1350000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+		echo 1350000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
 		echo 192000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-		echo 400000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+		echo 320000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk
 		echo 1 > /sys/devices/system/cpu/sched_mc_power_savings
 		echo -e "900\n1000\n1100\n" > /sys/devices/system/cpu/cpu0/cpufreq/gpu_mv_table
-		echo "192000 850000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "384000 875000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "486000 875000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "594000 875000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "702000 875000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "810000 887500" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "918000 900000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1026000 925000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1134000 937500" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1242000 950000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1350000 962500" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1458000 975000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1566000 1000000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1674000 1025000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
-		echo "1728000 1050000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "192000 860000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "384000 880000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "486000 880000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "594000 880000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "702000 880000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "810000 900000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "918000 920000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1026000 940000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1134000 960000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1242000 980000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1350000 1000000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1458000 1020000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1566000 1040000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1674000 1060000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
+		echo "1728000 1080000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
 	;;
 	"0")
 		echo "smartmax_eps" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -101,6 +101,6 @@ case "$powermode" in
 esac
 
 # Performance: CPU --> PVS3 & GPU --> Normal
-# Balance:     CPU --> PVS6 & GPU --> PowerSave
+# Balance:     CPU --> PVS-ME & GPU --> PowerSave
 # PowerSave:   CPU --> PVS6 & GPU --> PowerSaver
 

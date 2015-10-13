3ab016ce2fe34b485a2c1bd3e72594e86dc060b4
pexcn <pexcn@qq.com>
2015-10-13 22:47:19 +0800

    update powerprofile
    Change-Id: Ia416c421cff2e9cec3e0380b9588fcd6a52b1d6a

--- rootdir/etc/powerprofile.sh
+++ rootdir/etc/powerprofile.sh
@@ -39,15 +39,15 @@ case "$powermode" in
 		echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 		echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 		echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-		echo 1350000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-		echo 1350000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-		echo 1350000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-		echo 1350000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+		echo 1728000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+		echo 1728000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+		echo 1728000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+		echo 1728000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
 		echo 192000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-		echo 320000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+		echo 400000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk
 		echo 1 > /sys/devices/system/cpu/sched_mc_power_savings
 		echo -e "900\n1000\n1100\n" > /sys/devices/system/cpu/cpu0/cpufreq/gpu_mv_table
 		echo "192000 860000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
@@ -99,8 +99,4 @@ case "$powermode" in
 		echo "1728000 1050000" > /sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels
 	;;
 esac
-
-# Performance: CPU --> PVS3 & GPU --> Normal
-# Balance:     CPU --> PVS-ME & GPU --> PowerSave
-# PowerSave:   CPU --> PVS6 & GPU --> PowerSaver
 

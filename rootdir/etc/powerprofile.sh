1fbd0fa8d22ebe28c6057f86f2228fc6029cb0ef
pexcn <pexcn@qq.com>
2015-10-20 19:41:10 +0800

    update powerprofile values
    Change-Id: I2dd22627b7f392e26542f3876c86eb08674c8e99

--- rootdir/etc/powerprofile.sh
+++ rootdir/etc/powerprofile.sh
@@ -27,22 +27,22 @@ case "$powermode" in
 		echo 1728000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 		echo 1728000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
 		echo 1728000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-		echo 192000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-		echo 192000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-		echo 192000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-		echo 192000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+		echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+		echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+		echo 384000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+		echo 384000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
 		echo 400000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk
 		echo 1 > /sys/devices/system/cpu/sched_mc_power_savings
 	;;
 	"0")
-		echo "smartmax_eps" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-		echo "smartmax_eps" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-		echo "smartmax_eps" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-		echo "smartmax_eps" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-		echo 1242000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-		echo 1242000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-		echo 1242000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-		echo 1242000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+		echo "smartmax" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+		echo "smartmax" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+		echo "smartmax" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+		echo "smartmax" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+		echo 1350000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+		echo 1350000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+		echo 1350000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+		echo 1350000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
 		echo 192000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 		echo 192000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq

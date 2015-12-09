d94d5d0317a8954168607d34b5906c8b2d11f93f
pexcn <pexcn@qq.com>
2015-12-09 15:15:47 +0800

    Revert "aries: add powerprofile support"
    reverted this commit: dd9f7d84e0572524c3f41670a75cb1fff172ba4c

--- device.mk
+++ device.mk
@@ -142,10 +142,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
-# PowerProfile
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/powerprofile.sh:system/bin/powerprofile.sh
-
 # Prebuilts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/sounds/Forest.ogg:system/media/audio/alarms/Forest.ogg \
